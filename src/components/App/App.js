3339309ffc11aca5ad41c53c3d8fee9bb87763f6
Andrew Brenner <Andrew_Brenner@comcast.com>
2021-11-04 15:49:41 -0400

    tile selector mod4

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,6 +17,17 @@ class App extends Component{
     };
   }
 
+
+  handleNumTileChange(num) {
+    this.setState(
+      {
+        numTiles: num,
+        playing: false,
+        tiles: []
+      }
+    );
+  }
+
   startGame(numTiles) {
     this.setState((state) => {
       return {
@@ -73,7 +84,7 @@ class App extends Component{
       <header className="App-header">
         Turbo-Matcher
       </header>
-        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame}/>
+        <OptionsPanel handleNumTileChange={this.handleNumTileChange} playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame}/>
         <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
     </div>
   );
