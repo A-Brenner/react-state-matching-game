d262aaacae14a1bf09f3f28d9ee9b5094e8a77e0
Andrew Brenner <Andrew_Brenner@comcast.com>
2021-11-04 16:47:24 -0400

    GameContext added to TileSelector

--- refactor/components/TileSelector/TileSelector.js
+++ refactor/components/TileSelector/TileSelector.js
@@ -7,23 +7,29 @@ const TileSelector = (props) => {
 
   const [ref, hovered] = useHover();
 
-  const dropdown = (
-        <div className='tileSelectorContent' >
-          <div className='number' onClick={() => props.handleNumTileChange(4)}>4</div>
-          <div className='number' onClick={() => props.handleNumTileChange(16)}>16</div>
-          <div className='number' onClick={() => props.handleNumTileChange(36)}>36</div>
-        </div>
-  )
+  <GameContext.Consumer>
+  {
+    ({numTiles, handleNumTileChange}) => {
+      const dropdown = (
+            <div className='tileSelectorContent' >
+              <div className='number' onClick={() => handleNumTileChange(4)}>4</div>
+              <div className='number' onClick={() => handleNumTileChange(16)}>16</div>
+              <div className='number' onClick={() => handleNumTileChange(36)}>36</div>
+            </div>
+      )
 
-  return (
-    <div className='tileSelector'>
-      <div>Number of Tiles</div>
-      <div className='tileSelectorDropdown' ref={ref}>
-        {props.numTiles}
-        {hovered ? dropdown : null}
-      </div>
-    </div>
-  )
+      return (
+        <div className='tileSelector'>
+          <div>Number of Tiles</div>
+          <div className='tileSelectorDropdown' ref={ref}>
+            {props.numTiles}
+            {hovered ? dropdown : null}
+          </div>
+        </div>
+      )
+    }
+  }
+  </GameContext.Consumer>
 }
 
 export default TileSelector
