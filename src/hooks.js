c7478b37c26d90af0800d1fb082b8e9e23e56436
Andrew Brenner <Andrew_Brenner@comcast.com>
2021-11-04 15:19:10 -0400

    custom useHover Hook

---
+++ src/hooks.js
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from 'react'
+
+function useHover() {
+    const ref = useRef();
+    const [hovered, setHovered] = useState(false);
+    const enter = () => {
+        setHovered(true);
+    }
+    const leave = () => {
+        setHovered(false);
+    }
+    useEffect(() => {
+        const refCopy = ref;
+        refCopy.current.addEventListener('mouseenter', enter);
+        refCopy.current.addEventListener('mouseleave', () => leave);
+
+        return () => {
+            refCopy
+        }
+    });
+    return [ref, hovered, setHovered];
+}
+
+export default useHover;
